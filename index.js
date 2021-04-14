93d8e9148eafcdaa56c0011fe9bcc5ea1d4e7995
genebit <genebitara@gmail.com>
2021-04-14 22:51:28 +0800

    Finished fetching random api 👏👏

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
+console.log('API link: https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole');
+console.log('Copy and paste the link to fetch random data');
+
 let inputfield = document.getElementById('url');
-let fetch_button = document.getElementById('fetch-button');
+let fetchButton = document.getElementById('fetch-button');
 let output = document.getElementById('output');
 
-var url;
+let url;
 
-fetch_button.onclick = function () {
+fetchButton.onclick = function () {
      url = inputfield.value;
 
-     fetch(url)
-          .then((response) => response.json())
-          .then((user) => {
-               for (var i = 0; i < user.length; i++) {
-                    console.log(user[i]);
-                    output.innerHTML = JSON.stringify(user[i]);
-               }
-          })
-          .catch(cause => {
-               console.error('Fetching Error: ' + cause);
-          });
+     var fieldNotEmpty = (url !== '');
+
+     if (fieldNotEmpty) {
+          fetch(url)
+               .then((response) => response.json())
+               .then((user) => {
+                    var randomUser = JSON.parse(JSON.stringify(user[0]));
+                    var first = 'FIRST NAME:  ' + randomUser.first + '<br><br>';
+                    var last = 'LAST NAME:  ' + randomUser.last + '<br><br>';
+                    var email = 'EMAIL:  ' + randomUser.email + '<br><br>';
+                    var address = 'ADDRESS:  ' + randomUser.address + '<br><br>';
+                    var created = 'CREATED:  ' + randomUser.created + '<br><br>';
+
+                    output.innerHTML = first + last + email + address + created;
+               })
+               .catch(cause => {
+                    console.error('Fetching Error: ' + cause);
+               });
+     } else {
+          console.warn('Input field is empty.');
+     }
 }
