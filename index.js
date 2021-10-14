15667b6d3431801607236653f22d7cea0543235b
genebit <genebitara@gmail.com>
2021-10-14 22:03:58 +0800

    Updates

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-console.log('API link: https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole');
-console.log('Copy and paste the link to fetch random data');
+console.log(
+	"API link: https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole"
+);
+console.log("Copy and paste the link to fetch random data");
 
-let inputfield = document.getElementById('url');
-let fetchButton = document.getElementById('fetch-button');
-let output = document.getElementById('output');
+let inputfield = document.getElementById("url");
+let fetchButton = document.getElementById("fetch-button");
+let output = document.getElementById("output");
 
 let url;
 
 fetchButton.onclick = function () {
-     url = inputfield.value;
+	url = inputfield.value;
 
-     var fieldNotEmpty = (url !== '');
+	var fieldNotEmpty = url !== "";
 
-     if (fieldNotEmpty) {
-          console.log('Fetching...');
-          fetch(url)
-               .then((response) => response.json())
-               .then((user) => {
-                    console.log('Fetched');
-                    
-                    var randomUser = JSON.parse(JSON.stringify(user[0]));
-                    var first = 'FIRST NAME:  ' + randomUser.first + '<br><br>';
-                    var last = 'LAST NAME:  ' + randomUser.last + '<br><br>';
-                    var email = 'EMAIL:  ' + randomUser.email + '<br><br>';
-                    var address = 'ADDRESS:  ' + randomUser.address + '<br><br>';
-                    var created = 'CREATED:  ' + randomUser.created + '<br><br>';
+	if (fieldNotEmpty) {
+		console.log("Fetching...");
+		fetch(url)
+			.then((response) => response.json())
+			.then((user) => {
+				console.log("Fetched");
 
-                    output.innerHTML = first + last + email + address + created;
-               })
-               .catch(cause => {
-                    console.error('Fetching Error: ' + cause);
-               });
-     } else {
-          console.warn('Input field is empty.');
-     }
-}
+				var randomUser = JSON.parse(JSON.stringify(user[0]));
+				var first = "FIRST NAME:  " + randomUser.first + "<br><br>";
+				var last = "LAST NAME:  " + randomUser.last + "<br><br>";
+				var email = "EMAIL:  " + randomUser.email + "<br><br>";
+				var address = "ADDRESS:  " + randomUser.address + "<br><br>";
+				var created = "CREATED:  " + randomUser.created + "<br><br>";
+
+				output.innerHTML = first + last + email + address + created;
+			})
+			.catch((cause) => {
+				console.error("Fetching Error: " + cause);
+			});
+	} else {
+		console.warn("Input field is empty.");
+	}
+};
